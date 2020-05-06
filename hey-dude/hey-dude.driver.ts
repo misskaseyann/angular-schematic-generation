9d1eca17ebb673849abb4cb361ec046652822c20
Kasey Stowell <kasey.stowell@atomicobject.com>
2020-05-06 15:05:12 -0400

    remove folder

--- hey-dude/hey-dude.driver.ts
+++
@@ -1,6 +0,0 @@
-import { HeyDudeComponent } from './hey-dude.component';
-import { ComponentDriver } from 'angular-component-driver';
-
-export class HeyDudeComponentDriver extends ComponentDriver<HeyDudeComponent>{
-
-}
